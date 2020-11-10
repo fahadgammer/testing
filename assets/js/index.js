5c72ef030d1b5ccf751aa888bca5914dfb9740e9
fahad <ifahadiftikhar@gmail.com>
2020-11-10 14:21:01 +0500

    added front-end

---
+++ assets/js/index.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+      // chnaging the color of login 
+      $('#email').css('border','1px solid #F2A433');
+      $('#password').css('border','1px solid #F2A433');     
+      // end
+    
+})
